00345caf0bbe6ea963dd9ca8de97a95580383d1b
Lily Chung <lkdc@mit.edu>
2014-08-11 23:44:00 -0400

    choose template extension

--- templates/make_template.bash
+++ templates/make_template.bash
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+if [ $# -lt 3 ]
+then
+    echo "usage: $0 NAME WIDTH HEIGHT [EXTENSION=pbm]"
+fi
+
 name=$(basename "$1" .svg)
-inkscape -z -e >(convert - "${name}_${2}_${3}.pbm") -w "$2" -h "$3" "$1"
+if [ -z "$4" ]
+then
+    ext=pbm
+else
+    ext=$4
+fi
+
+inkscape -z -e >(convert - "${name}_${2}_${3}.${ext}") -w "$2" -h "$3" "$1"
