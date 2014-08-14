b45f9a2005bf5b83326672c3e2db75270f4f14db
Lily Chung <lkdc@mit.edu>
2014-08-14 12:07:25 -0400

    handle arguments and different directories correctly

--- templates/rasterize_template.bash
+++ templates/rasterize_template.bash
@@ -3,9 +3,11 @@
 if [ $# -lt 3 ]
 then
     echo "usage: $0 NAME WIDTH HEIGHT [EXTENSION=pbm]"
+    exit 1
 fi
 
 name=$(basename "$1" .svg)
+dir=$(dirname "$1")
 if [ -z "$4" ]
 then
     ext=pbm
@@ -13,4 +15,6 @@ else
     ext=$4
 fi
 
-inkscape -z -e >(convert - "${name}_${2}_${3}.${ext}") -w "$2" -h "$3" "$1"
+outpath="${dir}/${name}_${2}_${3}.${ext}"
+
+inkscape -z -e >(convert - "${outpath}") -w "$2" -h "$3" "$1"
